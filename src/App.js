9bffd150719c68df9c30b51bbf0f1569b3dbcb9a
nehapol <nehapol40@gmail.com>
2023-10-03 20:28:16 +0530

    resume added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
-import Footer from './components/footer';
+import "./App.css";
+// import "react-toastify/dist/ReactToastify.min.css";
+
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+import Footer from "./components/footer";
+import NavBar from "./components/navbar";
+import Resume from "./pages/Resume";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/Notfound";
+
+
+
+// import { ToastContainer } from "react-toastify";
+
 
 function App() {
   return (
-    <>
-   <Footer/>
-    </>
+    <Router>
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route exact path="/resume">
+              <Resume />
+            </Route>
+            <Route path="">
+              <NotFound />
+            </Route>
+          </Switch>
+          <Footer />
+    </Router>
   );
 }
 
--- src/components/footer.js
+++ src/components/footer.js
@@ -2,18 +2,23 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className="w-full md:flex justify-center bg-slate-100 p-5 text-center text-gray-400 space-x-2">
-      <div>Created by Vedant Daigavane</div>
-      <div>
+    <div className="w-full md:flex justify-center bg-gray-800 p-5 text-center text-gray-400 space-x-2">
+      <div className='text-blue-100'>Made with  <span className="font-thin" role="img" aria-label="emoji">
+            ❤️
+          </span>{" "} by Neha Pol</div>
+      <div className='relative group'>
         <a
-          href="https://github.com/daigavane70/react-porfolio"
+          href="https://github.com/nehapol1108/Portfolio-react.git"
           target={'_blank'}
           rel="noreferrer"
-          className="text-cyan-500"
+          className="text-blue-300"
         >
-          ( Repo Link )
+         
+     ( Repo Link ) <span className="absolute -bottom-0 right-0 w-0 h-0.5 bg-blue-400 group-hover:w-full group-hover:transition-all"></span>
+
         </a>
       </div>
+      
     </div>
   );
 }
